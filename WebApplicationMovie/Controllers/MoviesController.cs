97ca15000756d4e86afe56af3e417c1a86da21e7
Martin Lumban Gaol <martinlumbangaol880@gmail.com>
2022-09-12 23:01:23 +0700

    Add project files.

---
+++ WebApplicationMovie/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationMovie.Models;
+using WebApplicationMovie.ViewModels;
+
+namespace WebApplicationMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Taken 1"};
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
